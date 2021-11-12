c821b8ba742538bee59525f22e7391df79982035
Victor Turitzin <turitzinv@gmail.com>
2021-11-11 20:37:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Victor",
+    streetAddress: "11 LakeWood",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeTwo = {...employee};
+
+    employeeTwo[key] = value;
+
+    return employeeTwo;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeThree = {...employee};
+
+    delete employeeThree[key];
+
+    return employeeThree;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    
+    return employee;
+}
